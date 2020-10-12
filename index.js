4af2158b14df450e04a0f78e581004c8ec6cd64f
unknown <abiodun_flb@yahoo.com>
2020-10-12 21:46:08 +0100

    added the functionality js

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const year = document.getElementById('year');
-const countdown = document.getElementById('countdown');
-const days = document.getElementById('days');
-const hours = document.getElementById('hours');
-const minutes = document.getElementById('minutes');
-const seconds = document.getElementById('seconds');
+var year = document.getElementById('year');
+var countdown = document.getElementById('countdown');
+var days = document.getElementById('days');
+var hours = document.getElementById('hours');
+var minutes = document.getElementById('minutes');
+var seconds = document.getElementById('seconds');
 
-const currentYear = new Date().getFullYear();
+var currentYear = new Date().getFullYear();
 
-const newYearDay = new Date(`january 01 ${currentYear + 1} 00:00:00`);
+var newYearDay = new Date(`january 01 ${currentYear + 1} 00:00:00`);
 
 // const nextBirthday = new Date(`june 1 ${currentYear + 1} 00:00:00`);
 
 function updateCountDown(){
-    let currentTime = new Date();
-    let diff = newYearDay - currentTime;
-    let d = Math.floor(diff/1000/60/60/24);
-    let h = Math.floor(diff/1000/60/60) % 24;
-    let m = Math.floor(diff/1000/60) % 60;
-    let s = Math.floor(diff/1000) % 60;
+    var currentTime = new Date();
+    var diff = newYearDay - currentTime;
+    var d = Math.floor(diff/1000/60/60/24);
+    var h = Math.floor(diff/1000/60/60) % 24;
+    var m = Math.floor(diff/1000/60) % 60;
+    var s = Math.floor(diff/1000) % 60;
     
     days.innerHTML = d < 10 ? '0' + d : d;
     minutes.innerHTML = m < 10 ? '0' + m : m;
